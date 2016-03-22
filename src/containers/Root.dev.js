6eaa6d9fae14aff0063c42d2f0cf3cf722cb00d0
marcinkornek <marcinkornek@gmail.com>
2016-03-22 08:14:34 +0100

    Replace containers/App with containers/Root for dev and prod

---
+++ src/containers/Root.dev.js
@@ -0,0 +1,28 @@
+import React, { Component, PropTypes } from 'react'
+import { Provider }                    from 'react-redux'
+import { Router }                      from 'react-router'
+import { createRoutes }                from '../routes';
+import configureStore                  from '../store/configureStore'
+import DevTools                        from './DevTools'
+
+const store = configureStore()
+const routes = createRoutes(store);
+
+export default class Root extends Component {
+  render() {
+    const { store, history } = this.props
+    return (
+      <Provider store={store}>
+        <div>
+          <Router history={history} routes={routes} />
+          <DevTools />
+        </div>
+      </Provider>
+    )
+  }
+}
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
+}
