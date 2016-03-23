d6d0b1831b44ca425c3b01165b0afd56a69293a2
marcinkornek <marcinkornek@gmail.com>
2016-03-23 21:44:24 +0100

    Fix problem with header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,45 +1,41 @@
 import React, { PropTypes } from 'react'
 import Router, { Link }     from 'react-router'
-import { Nav, Navbar, NavDropdown, MenuItem } from 'react-bootstrap'
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 export default class Header extends React.Component {
   render() {
+    const { currentUser } = this.props;
 
-    var rightNav
-    if (this.props.currentUser.email) {
-      rightNav =
-        <Nav navbar pullRight>
-          <NavDropdown eventKey={1} title={this.props.currentUser.email} id="basic-nav-dropdown">
-            <MenuItem eventKey={1.1}><Link to='/account/user'>Account</Link></MenuItem>
-            <MenuItem divider />
-             <MenuItem eventKey={1.2}><a href='' onClick={this.handleLogout.bind(this)}>Logout</a></MenuItem>
-          </NavDropdown>
-        </Nav>
+    var navlinks = [
+      <LinkContainer to='/'><NavItem eventKey={1}>Home</NavItem></LinkContainer>,
+      <LinkContainer to='/users'><NavItem eventKey={2}>Users</NavItem></LinkContainer>,
+      <LinkContainer to='/rides'><NavItem eventKey={3}>Rides</NavItem></LinkContainer>
+    ]
+    if (currentUser.email) {
+      navlinks.push(
+        <NavDropdown eventKey={4} title={currentUser.email} id="basic-nav-dropdown">
+          <MenuItem eventKey={4.1}><Link to='/account/user'>Account</Link></MenuItem>
+          <MenuItem divider />
+           <MenuItem eventKey={4.2}><a href='' onClick={this.handleLogout.bind(this)}>Logout</a></MenuItem>
+        </NavDropdown>
+      )
     } else {
-      rightNav =
-        <Nav navbar pullRight>
-          <MenuItem eventKey={2}><Link to='/login'>Login</Link></MenuItem>
-          <MenuItem eventKey={3}><Link to='/register'>Register</Link></MenuItem>
-        </Nav>
+      navlinks.push(<LinkContainer to='/login'><NavItem eventKey={5}>Login</NavItem></LinkContainer>)
+      navlinks.push(<LinkContainer to='/register'><NavItem eventKey={6}>Register</NavItem></LinkContainer>)
     }
 
-    var leftNav =
-      <Nav>
-        <MenuItem><Link to='/'>Home</Link></MenuItem>
-        <MenuItem><Link to='/users'>Users</Link></MenuItem>
-        <MenuItem><Link to='/rides'>Rides</Link></MenuItem>
-      </Nav>
+    var Brand = <Link to='/'>Brand</Link>
 
     return (
-      <Navbar>
+      <Navbar fixedTop>
         <Navbar.Header>
           <Navbar.Brand>
-            News App
+            {Brand}
           </Navbar.Brand>
         </Navbar.Header>
         <Nav>
-          {leftNav}
-          {rightNav}
+          {_.map(navlinks, (n) => n)}
         </Nav>
       </Navbar>
     )
