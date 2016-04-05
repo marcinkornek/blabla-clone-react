7a748d6138b64049bec52f336ac813f57e002bbb
marcinkornek <marcinkornek@gmail.com>
2016-04-05 08:27:43 +0200

    Configure 'configureStore.prod.js'

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,11 +1,28 @@
 import { createStore, applyMiddleware } from 'redux'
+import * as storage     from 'redux-storage'
+import createEngine     from 'redux-storage-engine-localstorage';
 import thunk            from 'redux-thunk'
 import rootReducer      from '../reducers'
+import { routerMiddleware, push } from 'react-router-redux'
+
+const engine = createEngine('my-save-key');
+const sMiddleware = storage.createMiddleware(engine);
 
 export default function configureStore(history, initialState = {}) {
-  return createStore(
+  const rMiddleware = routerMiddleware(history)
+
+  const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, rMiddleware, sMiddleware)
   )
+
+  const load = storage.createLoader(engine);
+  load(store);
+
+  load(store)
+    .then((newState) => console.log('Loaded state:', newState))
+    .catch(() => console.log('Failed to load previous state'));
+
+  return store
 }
