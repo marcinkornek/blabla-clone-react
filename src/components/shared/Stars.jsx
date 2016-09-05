50522f45aad2cffea01bde2d52dc05a1f50e117a
marcinkornek <marcinkornek@gmail.com>
2016-09-05 08:08:32 +0200

    Refactor code style in Stars.jsx

--- src/components/shared/Stars.jsx
+++ src/components/shared/Stars.jsx
@@ -1,13 +1,9 @@
-import React, { PropTypes }        from 'react'
+import React, { Component, PropTypes } from 'react'
 import { Tooltip, OverlayTrigger } from 'react-bootstrap'
-import Icon                        from 'react-fa'
-import styles                      from '../../stylesheets/shared/Shared'
+import Icon from 'react-fa'
+import styles from '../../stylesheets/shared/Shared'
 
-export default class Stars extends React.Component {
-  constructor (props, context) {
-    super(props, context)
-  }
-
+export default class Stars extends Component {
   render() {
     const tooltipComfort = (
       <Tooltip id='tooltip-comfort'>{this.props.label}</Tooltip>
