98e808fe7f4710fcb8859789e1d2396dcf2ac89c
marcinkornek <marcinkornek@gmail.com>
2016-09-09 08:48:02 +0200

    Add not working 'notifications indicator' to 'Header'

--- src/components/HeaderRight.jsx
+++ src/components/HeaderRight.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router'
 import RaisedButton from 'material-ui/RaisedButton'
+import Badge from 'material-ui/Badge';
 import FlatButton from 'material-ui/FlatButton'
 import ContentAddBox from 'material-ui/svg-icons/content/add-box'
+import IconButton from 'material-ui/IconButton'
+import NotificationsIcon from 'material-ui/svg-icons/social/notifications'
 import ActionSearch from 'material-ui/svg-icons/action/search'
-import FontIcon from 'material-ui/FontIcon'
+import headerStyles from '../stylesheets/shared/Header'
 
 const styles = {
   button: {
@@ -16,16 +19,43 @@ const styles = {
     verticalAlign: 'middle',
     color: 'white',
     minWidth: 40
+  },
+  badgeStyle: {
+    padding: 0,
+    color: 'white',
+    top: -5,
+    right: -5
+  },
+  iconStyle: {
+    verticalAlign: 'middle',
+    color: 'white',
   }
 }
 
 const HeaderRight = () => (
   <div>
-    <FlatButton
-      icon={<ActionSearch />}
-      style={styles.searchButton}
-      containerElement={<Link to="/rides" />}
-    />
+    <Link to="/users" className="header__notifications-button">
+      <Badge
+        badgeContent={10}
+        primary={true}
+        badgeStyle={styles.badgeStyle}
+      >
+        <IconButton
+          tooltip="Notifications"
+          iconStyle={styles.iconStyle}
+        >
+          <NotificationsIcon />
+        </IconButton>
+      </Badge>
+    </Link>
+    <Link to="/rides">
+      <IconButton
+        tooltip="Search rides"
+        iconStyle={styles.iconStyle}
+      >
+          <ActionSearch />
+      </IconButton>
+    </Link>
     <RaisedButton
       label="Add ride"
       labelPosition="after"
