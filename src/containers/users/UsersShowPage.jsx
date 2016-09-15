48d0867e0224189b0f4acbb1dac114adbd5fdd9d
marcinkornek <marcinkornek@gmail.com>
2016-09-15 08:38:39 +0200

    Improve user show page

--- src/containers/users/UsersShowPage.jsx
+++ src/containers/users/UsersShowPage.jsx
@@ -1,11 +1,12 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux';
 import { Col } from 'react-bootstrap'
-import Timestamp from 'react-time'
+import Time from 'react-time'
 import Icon from 'react-fa'
 import * as actions from '../../actions/users';
 import CarsItem from '../../components/cars/CarsIndexPageItem'
 import RidesItem from '../../components/rides/RidesIndexSimplePageItem'
+import Avatar from 'material-ui/Avatar'
 
 class UsersShowPage extends Component {
   static PropTypes = {
@@ -19,68 +20,16 @@ class UsersShowPage extends Component {
     fetchUser(userId)
   }
 
-  render() {
-    const { isFetching, user, currentUserId } = this.props
-
-    var ridesList
-    if (user.rides_as_driver) {
-      ridesList = user.rides_as_driver.items.map((ride, i) =>
-        <RidesItem ride={ride} key={i} />
-      )
-    } else {
-      ridesList = 'No rides'
-    }
-
-    var userRidesAsDriver =
-      <div className='user-show'>
-        <div className='user-show__heading--activity'>
-          Rides as driver
-        </div>
-        {ridesList}
-      </div>
-
-    var userActivity =
-      <div className='user-show'>
-        <div className='user-show__heading--activity'>
-          Activity
-        </div>
-        <div className='user-show__details'>
-          <Icon name='clock-o' className='user-show__details-icon'/>
-          <div className='user-show__details-label'>Last visit</div>
-          <div className='user-show__details-value'><Timestamp value={user.last_seen_at} format="DD MMM YYYY - HH:mm" /></div>
-        </div>
-        <div className='user-show__details'>
-          <Icon name='calendar-times-o' className='user-show__details-icon'/>
-          <div className='user-show__details-label'>Joined</div>
-          <div className='user-show__details-value'><Timestamp value={user.created_at} format="DD MMM YYYY" /></div>
-        </div>
-      </div>
-
-    var carsList
-    if (user.cars) {
-      carsList = user.cars.items.map((car, i) =>
-        <CarsItem car={car} currentUserId={currentUserId} key={i} />
-      )
-    } else {
-      carsList = 'No cars'
-    }
-
-    var userCar =
-      <div className='user-show'>
-        <div className='user-show__heading--car'>
-          Cars
-        </div>
-        <div className='user-show__details-car'>
-          {carsList}
-        </div>
-      </div>
-
+  renderUserAge() {
+    const { user } = this.props
     if (user.age) {
-      var age =
-      <div className='main-info__details-age'>({user.age} years)</div>
+      return(<div className='main-info__details-age'>({user.age} years)</div>)
     }
+  }
 
-    var userInfo =
+  renderUserInfo() {
+    const { user } = this.props
+    return(
       <div className='user-show'>
         <div className='user-show__heading--info'>
           User
@@ -90,20 +39,94 @@ class UsersShowPage extends Component {
         </div>
         <div className='user-show__details-info'>
           <div className='user-show__details-name'>{user.full_name}</div>
-          <div className='user-show__details-age'>{age}</div>
+          <div className='user-show__details-age'>{this.renderUserAge()}</div>
           <div className='user-show__details-email'>{user.email}</div>
         </div>
       </div>
+    )
+  }
 
+  renderUserCar() {
+    return(
+      <div className='user-show'>
+        <div className='user-show__heading--car'>
+          Cars
+        </div>
+        <div className='user-show__details-car'>
+          {this.renderCarsList()}
+        </div>
+      </div>
+    )
+  }
+
+  renderCarsList() {
+    const { user, currentUserId } = this.props
+    if (user.cars) {
+      return(
+        user.cars.items.map((car, i) =>
+          <CarsItem car={car} currentUserId={currentUserId} key={i} />
+        )
+      )
+    } else {
+      return('No cars')
+    }
+  }
+
+  renderUserActivity() {
+    const { user } = this.props
+    return(
+      <div className='user-show'>
+        <div className='user-show__heading--activity'>
+          Activity
+        </div>
+        <div className='user-show__details'>
+          <Icon name='clock-o' className='user-show__details-icon'/>
+          <div className='user-show__details-label'>Last visit</div>
+          <div className='user-show__details-value'><Time value={user.last_seen_at || Date.now()} format="DD MMM YYYY - HH:mm" /></div>
+        </div>
+        <div className='user-show__details'>
+          <Icon name='calendar-times-o' className='user-show__details-icon'/>
+          <div className='user-show__details-label'>Joined</div>
+          <div className='user-show__details-value'><Time value={user.created_at || Date.now()} format="DD MMM YYYY" /></div>
+        </div>
+      </div>
+    )
+  }
+
+  renderUserRidesAsDriver() {
+    return(
+      <div className='user-show'>
+        <div className='user-show__heading--activity'>
+          Rides as driver
+        </div>
+        {this.renderRidesList()}
+      </div>
+    )
+  }
+
+  renderRidesList() {
+    const { user } = this.props
+    if (user.rides_as_driver) {
+      return(
+        user.rides_as_driver.items.map((ride, i) =>
+          <RidesItem ride={ride} key={i} />
+        )
+      )
+    } else {
+      return('No rides')
+    }
+  }
+
+  render() {
     return (
       <div className='show-grid'>
         <Col xs={8}>
-          {userInfo}
-          {userRidesAsDriver}
+          {this.renderUserInfo()}
+          {this.renderUserRidesAsDriver()}
         </Col>
         <Col xs={4}>
-          {userActivity}
-          {userCar}
+          {this.renderUserActivity()}
+          {this.renderUserCar()}
         </Col>
       </div>
     )
