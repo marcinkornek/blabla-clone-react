e96180b41d2e5f1a109a4f3958baef1859726493
marcinkornek <marcinkornek@gmail.com>
2016-09-26 17:12:45 +0200

    Improve constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,20 +1,21 @@
 var APIRoot
 if (__DEVELOPMENT__) {
-  APIRoot = 'http://localhost:3000'
+  APIRoot = 'http://localhost:3000/api/'
 } else {
-  APIRoot = 'https://blabla-clone-api.herokuapp.com'
+  APIRoot = 'https://blabla-clone-api.herokuapp.com/api/'
 }
 
 module.exports = {
+  APIRoot: APIRoot,
   APIEndpoints: {
-    USERS:         APIRoot + '/api/users',
-    CARS:          APIRoot + '/api/cars',
-    NOTIFICATIONS: APIRoot + '/api/notifications',
-    RIDES:         APIRoot + '/api/rides',
-    RIDE_REQUESTS: APIRoot + '/api/ride_requests',
-    SESSIONS:      APIRoot + '/api/sessions',
-    LOGIN_EMAIL:   APIRoot + '/api/sessions/login',
-    LOGIN_FB:      APIRoot + '/api/sessions/oath_login'
+    USERS:         APIRoot + 'users',
+    CARS:          APIRoot + 'cars',
+    NOTIFICATIONS: APIRoot + 'notifications',
+    RIDES:         APIRoot + 'rides',
+    RIDE_REQUESTS: APIRoot + 'ride_requests',
+    SESSIONS:      APIRoot + 'sessions',
+    LOGIN_EMAIL:   APIRoot + 'sessions/login',
+    LOGIN_FB:      APIRoot + 'sessions/oath_login'
   },
 
   Permissions: {
