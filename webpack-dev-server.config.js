3415b989c25e2b595b6b1c3930885bfcebee2089
marcinkornek <marcinkornek@gmail.com>
2016-09-26 17:12:13 +0200

    Update dependencies: babel to v6

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -32,14 +32,13 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js$/,
-        loaders: ['babel?optional[]=runtime&stage=0'],
-        include: path.join(__dirname, 'src'),
-      },
-      {
         test: /\.jsx?$/,
-        loaders: ['babel?optional[]=runtime&stage=0'],
-        include: path.join(__dirname, 'src')
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+          presets: ['react', 'es2015', 'stage-1', 'stage-0'],
+          plugins: ['transform-runtime']
+        }
       },
       {
         test: /\.cjsx?$/,
