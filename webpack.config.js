b4b72274e8a5bac2a17f22d29aa69539668a8690
marcinkornek <marcinkornek@gmail.com>
2016-11-11 16:04:41 +0100

    Move webpack configs to separate files and one main config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+var path = require('path')
+require('es6-promise').polyfill()
+
+module.exports = {
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1', 'stage-0'],
+          plugins: ['transform-runtime']
+        }
+      },
+      {
+        test: /\.(css)$/,
+        loaders: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(otf|eot|png|svg|ttf|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url?limit=8192'
+      },
+      {
+        test: /\.(json)$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.(scss)$/,
+        loader: 'style!css!sass?sourceMap'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.scss']
+  }
+};
---
+++ webpack/hot-dev.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+var config = require('./dev.config');
+
+config.devServer.hot = true
+
+config.entry.push(
+  'webpack/hot/only-dev-server',
+  'react-hot-loader/patch'
+);
+
+config.module = {
+  loaders: [
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1', 'stage-0'],
+        plugins: ['react-hot-loader/babel']
+      }
+    },
+    {
+      test: /\.(css)$/,
+      loaders: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.(otf|eot|png|svg|ttf|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: 'url?limit=8192'
+    },
+    {
+      test: /\.(json)$/,
+      loader: 'json-loader'
+    },
+    {
+      test: /\.(scss)$/,
+      loader: 'style!css!sass?sourceMap'
+    }
+  ]
+};
+
+config.plugins.push(
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin(),
+  new webpack.DefinePlugin({
+    __DEVELOPMENT__: true,
+    __DEVTOOLS__: true
+  })
+);
+
+module.exports = config;
