8d0e04e195df15b38d3792806738261b700bbe17
marcinkornek <marcinkornek@gmail.com>
2016-12-04 19:06:21 +0100

    Add missing 'redux-axios-middleware' to production config

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,7 +1,19 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import axios from 'axios'
+import axiosMiddleware from 'redux-axios-middleware'
 import rootReducer from '../reducers'
 import { routerMiddleware } from 'react-router-redux'
+import { APIRoot } from '../constants/constants'
+
+const client = axios.create({
+   baseURL: APIRoot,
+   headers: {
+     'Accept': 'application/vnd.blabla-clone-v1+json',
+     'Content-Type': 'application/json'
+   },
+   responseType: 'json'
+})
 
 export default function configureStore(history, initialState = {}) {
   const rMiddleware = routerMiddleware(history)
@@ -11,7 +23,8 @@ export default function configureStore(history, initialState = {}) {
     initialState,
     applyMiddleware(
       thunk,
-      rMiddleware
+      rMiddleware,
+      axiosMiddleware(client)
     )
   )
 
