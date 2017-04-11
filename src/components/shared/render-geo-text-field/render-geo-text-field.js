4850e1096a3e7da34b1531ae9bc2f40dbdd4ee7e
marcinkornek <marcinkornek@gmail.com>
2017-04-11 20:48:47 +0200

    Improve geosuggest field styles

--- src/components/shared/render-geo-text-field/render-geo-text-field.js
+++ src/components/shared/render-geo-text-field/render-geo-text-field.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Geosuggest from 'react-geosuggest'
+import classNames from 'classnames'
 
 export const renderGeoTextField = ({ input, label, meta: { touched, error }, initialValue, ...custom }) => {
   return (
-    <Geosuggest
-      inputClassName='form-control'
-      placeholder={label}
-      initialValue={initialValue ? initialValue : ''}
-      onSuggestSelect={(value) => input.onChange(value)} />
+    <div className={classNames('geosuggest__wrapper', {'geosuggest__wrapper--error' : touched && error})}>
+      <Geosuggest
+        placeholder={label}
+        initialValue={initialValue ? initialValue : ''}
+        onSuggestSelect={(value) => input.onChange(value)} />
+        <div className="geosuggest__error">{touched && error}</div>
+    </div>
   )
 }
